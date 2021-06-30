86d9bbe4c65967ba40f5b29601d62e1a9c098abb
lvoet <71518898+lvoet@users.noreply.github.com>
2021-06-30 16:28:17 -0700

    Update __init__.py

--- pyoptsparse/__init__.py
+++ pyoptsparse/__init__.py
@@ -10,12 +10,12 @@ from .pyOpt_optimizer import Optimizer
 from .pyOpt_optimizer import OPT
 
 # Now import all the individual optimizers
-from .pySNOPT.pySNOPT import SNOPT
+# from .pySNOPT.pySNOPT import SNOPT
 from .pyIPOPT.pyIPOPT import IPOPT
-from .pySLSQP.pySLSQP import SLSQP
-from .pyCONMIN.pyCONMIN import CONMIN
-from .pyPSQP.pyPSQP import PSQP
-from .pyNLPQLP.pyNLPQLP import NLPQLP
+# from .pySLSQP.pySLSQP import SLSQP
+# from .pyCONMIN.pyCONMIN import CONMIN
+# from .pyPSQP.pyPSQP import PSQP
+# from .pyNLPQLP.pyNLPQLP import NLPQLP
 # from .pyNSGA2.pyNSGA2 import NSGA2
-from .pyALPSO.pyALPSO import ALPSO
-from .pyParOpt.ParOpt import ParOpt
+# from .pyALPSO.pyALPSO import ALPSO
+# from .pyParOpt.ParOpt import ParOpt
