cb2f2c3a2ac3b44b4ccd5454f6bbaf0a7a1f1f96
lvoet <71518898+lvoet@users.noreply.github.com>
2021-06-30 16:28:45 -0700

    Update setup.py

--- pyoptsparse/setup.py
+++ pyoptsparse/setup.py
@@ -8,15 +8,15 @@ def configuration(parent_package="", top_path=None):
     config = Configuration("pyoptsparse", parent_package, top_path)
 
     # need: auto add_subpackage from source availability
-    config.add_subpackage("pySNOPT")
+#     config.add_subpackage("pySNOPT")
     config.add_subpackage("pyIPOPT")
-    config.add_subpackage("pySLSQP")
-    config.add_subpackage("pyCONMIN")
-    config.add_subpackage("pyNLPQLP")
+#     config.add_subpackage("pySLSQP")
+#     config.add_subpackage("pyCONMIN")
+#     config.add_subpackage("pyNLPQLP")
 #     config.add_subpackage("pyNSGA2")
-    config.add_subpackage("pyPSQP")
-    config.add_subpackage("pyALPSO")
-    config.add_subpackage("pyParOpt")
+#     config.add_subpackage("pyPSQP")
+#     config.add_subpackage("pyALPSO")
+#     config.add_subpackage("pyParOpt")
     config.add_subpackage("postprocessing")
     config.add_data_files("LICENSE")
 
